21c4423f6f02f0b102a53a1c18f9358128d15c61
Stewart Newnham <git@newnham.com>
2015-02-21 17:28:57 +0800

    Contact: Add extra tests for getter/setters

--- cw4/src/test/ContactTest.java
+++ cw4/src/test/ContactTest.java
@@ -13,9 +13,14 @@ public class ContactTest {
 	@Test
 	public void test() {
 		
-		Contact c = new ContactImpl("Harry", "He Likes A Drink");
-		assertEquals("Name Check: ", "Harry", c.getName());
-		assertEquals("Notes Check: ", "He Likes A Drink", c.getNotes());
+		ContactImpl c = new ContactImpl("Harry", "He Likes A Drink");
+		assertEquals("getName Check: ", "Harry", c.getName());
+		assertEquals("getNotes Check: ", "He Likes A Drink", c.getNotes());
+		
+		c.setName("Harriet");  
+		c.setNotes("Drinks White Wine");
+		assertEquals("setName Check", "Harriet", c.getName());
+		assertEquals("Drinks White Wine", c.getNotes());
 		
 	}
 
