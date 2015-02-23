889a111ba9fc3b6c45c29d66ffa76770f4d2ab41
Stewart Newnham <git@newnham.com>
2015-02-24 03:54:51 +0800

    IdGenerator Test - Run 10,000s of tests to check ID ranges are correct

---
+++ cw4/src/impls/IdGeneratorTest.java
@@ -0,0 +1,33 @@
+package impls;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IdGeneratorTest {
+
+	@Test
+	public void testContactID() {
+		String c = "contactId";
+		int min = 10000;
+		int max = 100000;
+		for(int i=0;i<10000;i++){
+			int x = IdGenerator.generateID(c);
+			assertTrue("contactID Generator - min ID: ", x >= min);
+			assertTrue("contactID Generator - max ID: ", x <= max);
+		}
+	}
+	
+	
+	public void testMeetingID() {
+		String m = "meetingId";
+		int min = 100000;
+		int max = 1000000;
+		for(int i=0;i<1000000;i++){
+			int x = IdGenerator.generateID(m);
+			assertTrue("contactID Generator - min ID: ", x >= min);
+			assertTrue("contactID Generator - max ID: ", x <= max);
+		}
+	}
+
+}
