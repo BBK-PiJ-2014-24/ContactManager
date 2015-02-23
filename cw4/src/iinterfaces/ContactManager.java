50d04ed3d589032fd809cd0d25d4b01b0c9039a1
Stewart Newnham <git@newnham.com>
2015-02-23 18:05:16 +0800

    ContactManager Interface - Changed Mind: Uncheck and Reveal getFutureMeeting() and Check and re-hide getPastMeeting().

--- cw4/src/iinterfaces/ContactManager.java
+++ cw4/src/iinterfaces/ContactManager.java
@@ -24,14 +24,14 @@ public interface ContactManager { /**
 * @return the meeting with the requested ID, or null if it there is none.
 * @throws IllegalArgumentException if there is a meeting with that ID happening in the future
 */
-PastMeeting getPastMeeting(int id);
+// PastMeeting getPastMeeting(int id);
 
 /**
 * Returns the FUTURE meeting with the requested ID, or null if there is none. *
 * @param id the ID for the meeting
 * @return the meeting with the requested ID, or null if it there is none.
 * @throws IllegalArgumentException if there is a meeting with that ID happening in the past */
-//FutureMeeting getFutureMeeting(int id);
+FutureMeeting getFutureMeeting(int id);
 
 /**
 * Returns the meeting with the requested ID, or null if it there is none
