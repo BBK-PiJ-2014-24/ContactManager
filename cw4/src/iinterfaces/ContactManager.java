632879f4590cc1b4a5b006f2c0a8ecef4d4e43c5
Stewart Newnham <git@newnham.com>
2015-03-01 16:52:47 +0800

    ContactManager Interface - Reveal/Uncheck getFutureMeetingLits(Contact)

--- cw4/src/iinterfaces/ContactManager.java
+++ cw4/src/iinterfaces/ContactManager.java
@@ -37,7 +37,7 @@ public interface ContactManager { /**
 * Returns the meeting with the requested ID, or null if it there is none
 * @param id the ID for the
 * @return the meeting with the requested ID, or null if it there is none.*/
-Meeting getMeeting(int id);
+   Meeting getMeeting(int id);
 
 /**
 * Returns the list of future meetings scheduled with this contact. *
@@ -47,7 +47,7 @@ Meeting getMeeting(int id);
 * @param contact one of the user’s contacts
 * @return the list of future meeting(s) scheduled with this contact
 * @throws IllegalArgumentException if the contact does not exist */
-//List<Meeting> getFutureMeetingList(Contact contact);
+List<Meeting> getFutureMeetingList(Contact contact);
 
 /**
 * Returns the list of meetings that are scheduled for, or that took
