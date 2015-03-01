d7acaf62ac746804c962ae34ff5b1e56d4e19382
Stewart Newnham <git@newnham.com>
2015-03-01 18:11:18 +0800

    ContactManager Test - Set up tests for getFutureMeetingList(Contact) including Exception.

--- cw4/src/test/ContactManagerTest.java
+++ cw4/src/test/ContactManagerTest.java
@@ -32,20 +32,45 @@ public class ContactManagerTest {
 	Calendar calPast;
 	Calendar calFut;
 	Calendar badDate;
+	Calendar mar15;
+	Calendar apr15;
+	Calendar may15;
+	Calendar jun15;
+	Calendar jul15;
+	Calendar aug15;
 	SimpleDateFormat sdf;
 	Contact c1;
 	Contact c2;
 	Contact c3;
+	Contact jim;
+	Contact c5;
+	Contact gertrude;
 	Set<Contact> contactSet;
 	Set<Contact> emptyContactSet;
+	Set<Contact>JimJillSet;
+	Set<Contact>HarrySophieSet;
+	
+	List<Meeting> jimMeetingList;
+	List<Meeting> harryMeetingList;
+	
 	ContactManagerImpl cm;
 	FutureMeetingImpl futMeetGood;
 	FutureMeetingImpl futMeetBad;
 	PastMeetingImpl pastMeetGood;
 	PastMeetingImpl pastMeetBad;
 	Meeting m;
+	
+	FutureMeeting meetingMar15;
+	FutureMeeting meetingApr15;
+	FutureMeeting meetingMay15;
+	FutureMeeting meetingJun15;
+	FutureMeeting meetingJul15;
+	FutureMeeting meetingAug15;
+	
 	String notes;
 	String badNotes;
+	
+	
 	
 	@Before
 	public void setUp(){
@@ -55,12 +80,23 @@ public class ContactManagerTest {
 		calFut = new GregorianCalendar(2018, 11, 25, 16, 37);
 		sdf = new SimpleDateFormat("dd-MM-yyyy hh:mm");
 		badDate = null;
+		mar15 = new GregorianCalendar(2015, 3, 23, 12, 05);
+		apr15 = new GregorianCalendar(2015, 4, 23, 12, 05);
+		may15 = new GregorianCalendar(2015, 5, 23, 12, 05);
+		jun15 = new GregorianCalendar(2015, 6, 23, 12, 05);
+		jul15 = new GregorianCalendar(2015, 7, 23, 12, 05);
+		aug15 = new GregorianCalendar(2015, 8, 23, 12, 05);
+		
 				
 		// Create Contacts
 		// ------------
 		 c1 = new ContactImpl("Harry","Likes a Drink");
 		 c2 = new ContactImpl("Jill","Likes White Wine");
 		 c3 = new ContactImpl("Jack", "Teetotal");
+		 jim = new ContactImpl("Jim", "BUSY MAN");
+		 c5 = new ContactImpl("Sophie", "Angel");
+		 gertrude = new ContactImpl("Gertrude", "NOT ON CONTACT LIST");
+		 
 		 
 		
 		// Create Set of Contacts
@@ -69,8 +105,16 @@ public class ContactManagerTest {
 		contactSet.add(c1);
 		contactSet.add(c2);
 		contactSet.add(c3);
-		
+	
 		emptyContactSet = null;
+		
+		JimJillSet = new HashSet<Contact>();
+		JimJillSet.add(c2);
+		JimJillSet.add(jim);
+		
+		HarrySophieSet = new HashSet<Contact>();
+		HarrySophieSet.add(c1);
+		HarrySophieSet.add(c5);
 		
 		// Instant ContactManager
 		// ----------------------
@@ -78,14 +122,13 @@ public class ContactManagerTest {
 		
 		// Instant Meetings
 		// ----------------
-		
-		
-	//	futMeetBad = new FutureMeetingImpl(contactSet, calPast);   // FutureMeeting w/Past Date 
-	//	m = new MeetingImpl(contactSet, calPast);
-	//	pastMeetGood = new PastMeetingImpl(contactSet, calPast, notes); // PastMeeting w/Past Date 
-	//	pastMeetBad = new PastMeetingImpl(contactSet, calFut, notes); // PasteMeeting w/Future Date
-		
-		
+
+		 
+		 // MeetingList
+		 // -----------
+
+
+
 		// Notes
 		// -----
 		
@@ -122,8 +165,8 @@ public class ContactManagerTest {
 	}
 	
 	@Test
-	// Tests for getFutureMeeting()
-	// ----------------------------
+	// Tests for getFutureMeeting(id)
+	// ------------------------------
 	public void testGetFutureMeeting(){
 		int id = cm.addFutureMeeting(contactSet, calFut);
 		futMeetGood = new FutureMeetingImpl(id, contactSet, calFut);  // FutureMeeting w/Future Date
@@ -158,8 +201,8 @@ public class ContactManagerTest {
 	}
 	
 	@Test
-	// Test for getPastMeeting()
-	// -------------------------
+	// Test for getPastMeeting(id)
+	// ---------------------------
 	public void testGetNewPastMeeting(){
 		cm.addNewPastMeeting(contactSet, calPast, notes);
 		int id = cm.getLastIdUpdate(); // Retrieve the randomly generated ID
@@ -206,5 +249,44 @@ public class ContactManagerTest {
 		int id = 1234;
 		assertEquals("test getMeeting() - Check Bad id", null, cm.getMeeting(id));	
 	}
+	
+	@Test
+	// Test for getFutureMeetingList(Contact)
+	// --------------------------------------
+	public void testGetFutureMeetingContact(){
+		 int idMar15 = cm.addFutureMeeting(JimJillSet, mar15);  // add meetings to ContactManager
+		 int idApr15 = cm.addFutureMeeting(JimJillSet, apr15);
+		 int idMay15 = cm.addFutureMeeting(JimJillSet, may15);
+		 int idJun15 = cm.addFutureMeeting(JimJillSet, jun15);
+		 int idJul15 = cm.addFutureMeeting(HarrySophieSet, jul15);
+		 int idAug15 = cm.addFutureMeeting(HarrySophieSet, aug15);
+		 
+		 meetingMar15 = new FutureMeetingImpl(idMar15,JimJillSet, mar15); // instant test Fut Meetings
+		 meetingApr15 = new FutureMeetingImpl(idApr15,JimJillSet, apr15);
+		 meetingMay15 = new FutureMeetingImpl(idMay15,JimJillSet, may15);
+		 meetingJun15 = new FutureMeetingImpl(idJun15,JimJillSet, jun15);
+		 meetingJul15 = new FutureMeetingImpl(idJul15,HarrySophieSet, jul15);
+		 meetingAug15 = new FutureMeetingImpl(idAug15,HarrySophieSet, aug15);
+		 
+		 jimMeetingList = new ArrayList<Meeting>();    // Create a test List for Contacts Jim, harry
+		 jimMeetingList.add(meetingMar15);
+		 jimMeetingList.add(meetingApr15);
+		 jimMeetingList.add(meetingMay15);
+		 jimMeetingList.add(meetingJun15);
+		  
+		 harryMeetingList = new ArrayList<Meeting>();
+		 harryMeetingList.add(meetingJul15);
+		 harryMeetingList.add(meetingAug15);
+		 
+		 assertEquals("test getFutureMeeting(Contact)I: ", jimMeetingList, cm.getFutureMeetingList(jim));
+		 assertEquals("test getFutureMeeting(Contact)II: ", jimMeetingList, cm.getFutureMeetingList(jim));
+	}
+	
+	@Test
+	public void testEXGetFutureMeetingContact(){
+		ex.expect(IllegalArgumentException.class);
+		cm.getFutureMeetingList(gertrude);
+	}
+	
 	
 }
