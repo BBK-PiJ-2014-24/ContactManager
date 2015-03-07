998bd811be23b00959381ef1f6689064ca69734b
Stewart Newnham <git@newnham.com>
2015-03-08 02:35:57 +0800

    Contact Test - Added a toString() test (for I/O use)

--- cw4/src/test/ContactTest.java
+++ cw4/src/test/ContactTest.java
@@ -13,18 +13,17 @@ public class ContactTest {
 	@Test
 	public void test() {
 		
-		ContactImpl c = new ContactImpl("Harry", "He Likes A Drink");
+		ContactImpl c = new ContactImpl(1234, "Harry", "He Likes A Drink");
 		assertEquals("getName Check: ", "Harry", c.getName());
 		assertEquals("getNotes Check: ", "He Likes A Drink", c.getNotes());
-		//c.id = 1234;
-		//assertEquals("getID: ", 1234, c.getId());
+		assertEquals("getID Check: ", 1234, c.getId());
 		
 		c.setName("Harriet");  
 		c.addNotes("Drinks White Wine");
 		assertEquals("setName() Check", c.getName(), "Harriet");
 		assertEquals("getName() Check", c.getNotes(), "Drinks White Wine");
 		
-		
+		assertEquals("toString() Check: ", "1234,Harry,He Likes A Drink", c.toString());
 		
 		
 	}
